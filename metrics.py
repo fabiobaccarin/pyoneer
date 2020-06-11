6dc37c0af211ec36209e2a9fbd3e7c91d3633e41
Fabio Baccarin <fabiobaccarin@euler.local>
2020-06-11 15:28:35 -0300

    finish PValue class

--- metrics.py
+++ metrics.py
@@ -142,194 +142,164 @@ def cramerV(x: pd.Series, y: pd.Series) -> float:
     return np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
 
 
+def odds_ratio(self, x: pd.Series, y: pd.Series) -> float:
+    ''' Calculates the odds ratio for two categorical variables x and y. If
+        x and y are both binary with values 0 and 1, let T be the contingency
+        matrix of those variables (which is 2 x 2). Then the odds ratio is
+        the ratio of the product of T's main diagonal entries over the
+        product of T's secondary diagonal entries
+        
+        Parameters
+        ----------
+        x: pandas.Series
+            Variable
+            
+        y: pandas.Series
+            Variable
+            
+        Returns
+        -------
+        float:
+            Odds ratio
+    '''
+    tab = pd.crosstab(x, y)
+    return np.prod(np.diag(tab)) / np.prod(np.diag(np.fliplr(tab)))
+
+
 def yule_q(odds_ratio: float) -> float:
+    ''' Yule's Q statistic for measuring association between categorical
+        variables
+        
+        Parameters
+        ----------
+        odds_ratio: float
+            Value of odds ratio
+            
+        Returns
+        -------
+        float:
+            Value of Yule's Q
+    '''
+    
     return (odds_ratio - 1) / (odds_ratio + 1)
 
 
 class PValue:
-    """ Calculates p-values for variables to use for feature selection and
-        ranking. It uses the following conventions:
-            1. If the response is categorical and the predictor is also
-               categorical, the p-value refers to the result of a Fisher's
-               exact test
-            2. If the response is categorical and the predictor is continuous,
-               the p-value refers to the result of a Welch test where the
-               null hypothesis is that the mean value of the predictor is
-               the same for both classes of the response
-            3. If the response is continuous and the predictor is categorical,
-               the p-value refers to the result of a Welch test where the
-               null hypothesis is that the mean value of the response is the
-               same in both classes of the predictor
+    ''' Class grouping utilities for dealing with p-values '''
     
-        Parameters
-        ----------
-        size: int, default None
-            Size of samples to use for bootstrapping
-            
-        samples: int, default None
-            Number of repetitions of bootstrap to perform
-            
-        Attributes
-        ----------
-        
-        y_type: str, ('continous', 'categorical')
-            Type of response variable
-            
-        catvars: list of str
-            List of variables that should be treated as categorical/dummy
-            variables
-            
-        diff_means: pandas.Series
-            Difference in means used in tests
-            
-        odds_ratio: pandas.Series
-            Odds ratios used in tests
-        
-        pvalue: pandas.Series
-            P-values for variables in X. The variable names are the indexes
-            of the series
-            
-        pvalue_ci: pandas.DataFrame
-            P-values for variables in X with bootstrapped confidence intervals
-    """
-    
-    def __init__(self, size: int=None, samples: int=None):
-        self.size = size
-        self.samples = samples
-    
-    def _cat_cat_pair(self, x: str) -> bool:
-        return x in self.catvars and self.y_categorical
-    
-    def _cont_cat_pair(self, x: str) -> bool:
-        return x not in self.catvars and self.y_categorical
-    
-    def _cat_cont_pair(self, x: str) -> bool:
-        return x in self.catvars and not self.y_categorical
-    
-    def _fisher_exact(self, X: pd.Series, y: pd.Series) -> (float, float):
-        return ss.fisher_exact(pd.crosstab(X, y))
-    
-    def _welch(self, X: pd.Series, y: pd.Series) -> (float, float):
-        return ss.ttest_ind(X[y == 0], X[y == 1], equal_var=False)
-    
-    def _pvalue(self, X: pd.Series, y: pd.Series) -> float:
-        if self._cat_cat_pair(X.name):
-            _, p = self._fisher_exact(X, y)
-        elif self._cont_cat_pair(X.name):
-            _, p = self._welch(X, y)
-        elif self._cat_cont_pair(X.name):
-            _, p = self._welch(y, X)
-        
-        return p
-    
-    def _odds_ratio(self, X: pd.Series, y: pd.Series) -> float:
-        tab = pd.crosstab(X, y)
-        return np.prod(np.diag(tab)) / np.prod(np.diag(np.fliplr(tab)))
-    
-    def _diff_means(self, X: pd.Series, y: pd.Series) -> float:
-        return X[y == 1].mean() - X[y == 0].mean()
-        
-    def _setup(self, X: pd.DataFrame, y: pd.Series, y_categorical: bool,
-               catvars: list) -> None:
-        guards.not_dataframe(X, 'X')
-        guards.not_series(y, 'y')
-        guards.not_iterable(catvars, 'catvars')
-        if catvars == [] and not y_categorical:
-            raise ValueError('Cannot compute for continuous variables only. Either y must be categorical or some column in X must be listed in `catvars`')
-        
-        self._cols = X.columns.to_list()
-        
-        self.y_categorical = y_categorical
-        self.catvars = catvars
-    
-    def fit(self, X: pd.DataFrame, y: pd.Series, y_categorical: bool,
-            catvars: list=[]) -> None:
-        """ Fits to the attribute matrix X and response y
+    @staticmethod
+    def bonferroni(pvals: pd.Series, alpha: float=.05) -> pd.Series:
+        ''' Bonferroni correction for p-values
         
             Parameters
             ----------
-            X: pandas.DataFrame
-                Matrix of attributes
+            pvals: pandas.Series
+                Vector of p-values to transform
                 
-            y: pandas.Series
-                Vector of response (aka target, dependent variable) values to
-                use for testing hypothesis
+            alpha: float, default 0.05
+                Level of significance for rejecting the null hypothesis
                 
-            y_categorical: bool
-                Whether y is a categorical response
-                
-            catvars: list of str, default []
-                List of categorical variable names. They should be already
-                encoded as dummy variables
-        """
+            Returns
+            -------
+            pandas.Series:
+                Whether the null hypothesis is rejected for the corresponding
+                p-value
+        '''
         
-        self._setup(X, y, y_categorical, catvars)
+        guards.not_series(pvals, 'pvals')
         
-        self.pvalue = X.apply(self._pvalue, args=(y,))
-        self.pvalue.name = 'pvalue'
-        
-        self.diff_means = (X[[col for col in self._cols
-                             if self._cont_cat_pair(col)]]
-                           .apply(self._diff_means, args=(y,)))
-        self.diff_means.name = 'diff_means'
-        
-        if self.catvars != []:
-            self.odds_ratio = (X[[col for col in self.catvars
-                                 if self.y_categorical]]
-                               .apply(self._odds_ratio, args=(y,)))
-            self.odds_ratio.name = 'odds_ratio'
-
-
-    def fit_ci(self, X: pd.DataFrame, y: pd.Series, y_categorical: bool,
-               catvars: list=[], weights: pd.Series=None) -> None:
-        """ Fits to the attribute matrix X and response y, calulating
-            confidence infervals using bootstrapping
+        return pvals < alpha/len(pvals)
+    
+    @staticmethod
+    def sidak(pvals: pd.Series, alpha: float=.05) -> pd.Series:
+        ''' Sidak correction for p-values
         
             Parameters
             ----------
-            X: pandas.DataFrame
-                Matrix of attributes
+            pvals: pandas.Series
+                Vector of p-values to transform
                 
-            y: pandas.Series
-                Vector of response (aka target, dependent variable) values to
-                use for testing hypothesis
+            alpha: float, default 0.05
+                Level of significance for rejecting the null hypothesis
                 
-            y_categorical: bool
-                Whether y is a categorical response
-                
-            catvars: list of str, default []
-                List of categorical variable names. They should be already
-                encoded as dummy variables
-                
-            weights: pandas.Series, default None
-                If specified, it is a vector of weights to sample. Observations
-                with higher values in this vector are more likely to be
-                sampled
-        """
+            Returns
+            -------
+            pandas.Series:
+                Whether the null hypothesis is rejected for the corresponding
+                p-value
+        '''
         
-        self._setup(X, y, y_categorical, catvars)
-        guards.is_none(self.size, 'size')
-        guards.is_none(self.samples, 'samples')
+        guards.not_series(pvals, 'pvals')
         
-        vals = []
-        for s in range(self.samples):
-            X_s = X.sample(self.size, weights=weights, replace=True)
-            y_s = y[X.index]
-            vals.append(X_s[self._cols].apply(self._pvalue, args=(y_s,)).T)
-        self.pvalue_ci = pd.DataFrame(vals)
+        return pvals < 1 - (1 - alpha)**(1/len(pvals))
+    
+    @staticmethod
+    def holm_bonferroni(pvals: pd.Series, alpha: float=.05) -> pd.Series:
+        ''' Holm-Bonferroni correction for p-values
+        
+            Parameters
+            ----------
+            pvals: pandas.Series
+                Vector of p-values to transform
+                
+            alpha: float, default 0.05
+                Level of significance for rejecting the null hypothesis
+                
+            Returns
+            -------
+            pandas.Series:
+                Whether the null hypothesis is rejected for the corresponding
+                p-value
+        '''
+        
+        guards.not_series(pvals, 'pvals')
+        
+        vals = pvals.sort_values().reset_index(drop=True)
+        vals = pd.concat([vals, vals.index.to_series()], axis=1)
+        
+        return vals[0] < alpha/(len(pvals) - vals[1])
+    
+    @staticmethod
+    def holm_sidak(pvals: pd.Series, alpha: float=.05) -> pd.Series:
+        ''' Holm-Sidak correction for p-values
+        
+            Parameters
+            ----------
+            pvals: pandas.Series
+                Vector of p-values to transform
+                
+            alpha: float, default 0.05
+                Level of significance for rejecting the null hypothesis
+                
+            Returns
+            -------
+            pandas.Series:
+                Whether the null hypothesis is rejected for the corresponding
+                p-value
+        '''
+        
+        guards.not_series(pvals, 'pvals')
+        
+        vals = pvals.sort_values().reset_index(drop=True)
+        vals = pd.concat([vals, vals.index.to_series()], axis=1)
+        
+        return vals[0] < 1 - (1 - alpha)**(1/(len(pvals)-vals[1]))
         
     @staticmethod
-    def scale(vals: pd.Series) -> pd.Series:
-        """ Returns the negative of the p-values in a log 10 scale
+    def scale(pvals: pd.Series) -> pd.Series:
+        ''' Returns the negative of the p-values in a log 10 scale
         
             Parameters
             ----------
-            vals: pandas.Series
-                vector of p-values to transform
+            pvals: pandas.Series
+                Vector of p-values to transform
                 
             Returns
             -------
             pandas.Series:
                 Transformed p-values
-        """
+        '''
+        
+        guards.not_series(pvals, 'pvals')
+        
         return -np.log10(vals)
